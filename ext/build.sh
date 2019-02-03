5628c88999512a8c96980e80b61496d8029b2636
campisano <riccardo.campisano@gmail.com>
2019-02-03 16:59:16 -0200

    cleanup get.sh and build.sh scripts

--- ext/build.sh
+++ ext/build.sh
@@ -12,31 +12,31 @@ set -o errtrace -o pipefail
 
 fn_build_cmake()
 {
-    BASEDIR=$1
-    NAME=$2
-    DIR=$3
-    CMAKEARGS=$4
-    MAKETARGET=$5
+    NAME=$1
+    DIR=$2
+    CMAKE_ARGS=$3
+    MAKE_TARGET=$4
+    BASE_DIR=`pwd`
     echo -e " * \\033[1;33m${NAME}\\033[0;39m"
-    mkdir "${BASEDIR}/${DIR}/build"
-    cd "${BASEDIR}/${DIR}/build"
-    cmake ${CMAKEARGS} | grep -v -- '-- Detecting C' | grep -v -- '-- Check for working C' | grep -v -- '-- Looking for '
-    make ${MAKETARGET}
-    cd "${BASEDIR}"
+    mkdir "${BASE_DIR}/${DIR}/build"
+    cd "${BASE_DIR}/${DIR}/build" && \
+    cmake ${CMAKE_ARGS} | grep -v -- '-- Detecting C' | grep -v -- '-- Check for working C' | grep -v -- '-- Looking for ' && \
+    make ${MAKE_TARGET}
+    cd "${BASE_DIR}"
     echo
 }
 
-PWD=`pwd`
-
 rm -rf cpr
 rm -rf src/cpr-1.3.0/build
-fn_build_cmake "$PWD" "cpr" "src/cpr-1.3.0" "-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=11 -DUSE_SYSTEM_CURL=ON -DBUILD_CPR_TESTS=OFF -DCMAKE_INSTALL_PREFIX=../../../cpr .." ""
+fn_build_cmake "cpr" "src/cpr-1.3.0" "-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=11 -DUSE_SYSTEM_CURL=ON -DBUILD_CPR_TESTS=OFF -DCMAKE_INSTALL_PREFIX=../../../cpr .." ""
 mkdir cpr
 cp -a src/cpr-1.3.0/include cpr
 cp -a src/cpr-1.3.0/build/lib cpr
 
 rm -rf date sqlpp11
 rm -rf src/date-2.4.1/build/ src/sqlpp11-0.57/build/ src/sqlpp11-connector-postgresql_5e834b3/build/
-fn_build_cmake "$PWD" "HinnantDate" "src/date-2.4.1" "-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_MESSAGE=NEVER -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=../../../date .." install
-fn_build_cmake "$PWD" "sqlpp11" "src/sqlpp11-0.57" "-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_MESSAGE=NEVER -DHinnantDate_ROOT_DIR=../../../date -DENABLE_TESTS=OFF -DCMAKE_INSTALL_PREFIX=../../../sqlpp11 .." install
-fn_build_cmake "$PWD" "sqlpp11-connector-postgresql" "src/sqlpp11-connector-postgresql_5e834b3" "-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_MESSAGE=NEVER -DHinnantDate_ROOT_DIR=../../../date -DPostgreSQL_TYPE_INCLUDE_DIR=/usr/include/postgresql -DENABLE_TESTS=OFF -DCMAKE_INSTALL_PREFIX=../../../sqlpp11 .." install
+fn_build_cmake "HinnantDate" "src/date-2.4.1" "-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_MESSAGE=NEVER -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=../../../date .." install
+
+fn_build_cmake "sqlpp11" "src/sqlpp11-0.57" "-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_MESSAGE=NEVER -DHinnantDate_ROOT_DIR=../../../date -DENABLE_TESTS=OFF -DCMAKE_INSTALL_PREFIX=../../../sqlpp11 .." install
+
+fn_build_cmake "sqlpp11-connector-postgresql" "src/sqlpp11-connector-postgresql_5e834b3" "-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_MESSAGE=NEVER -DHinnantDate_ROOT_DIR=../../../date -DPostgreSQL_TYPE_INCLUDE_DIR=/usr/include/postgresql -DENABLE_TESTS=OFF -DCMAKE_INSTALL_PREFIX=../../../sqlpp11 .." install
--- ext/get.sh
+++ ext/get.sh
@@ -10,32 +10,36 @@ fn_abort()
 trap fn_abort ERR
 set -o errtrace -o pipefail
 
-fn_get_zip()
+fn_get()
 {
-    BASE_DIR=$1
+    NAME=$1
     URL=$2
-    cd "${BASE_DIR}/src"
-    rm -f tmp.zip
+    BASE_DIR=`pwd`
+    mkdir -p "${BASE_DIR}/src"
     echo -e " * \\033[1;33m${URL}\\033[0;39m"
-    curl -s "${URL}" --output tmp.zip
-    unzip -q tmp.zip
-    rm -f tmp.zip
+    cd "${BASE_DIR}/src" && curl -L -s "${URL}" -o "${NAME}"
     cd "${BASE_DIR}"
 }
 
-PWD=`pwd`
-mkdir -p src
+fn_get_zip()
+{
+    URL=$1
+    BASE_DIR=`pwd`
+    fn_get tmp.zip "${URL}"
+    cd "${BASE_DIR}/src" && unzip -q tmp.zip && rm -f tmp.zip
+    cd "${BASE_DIR}"
+}
 
-fn_get_zip "$PWD" https://codeload.github.com/campisano/GenericMakefile/zip/master
+fn_get_zip https://codeload.github.com/campisano/GenericMakefile/zip/master
 cp -a src/GenericMakefile-master/Makefile.inc .
 
-fn_get_zip "$PWD" https://codeload.github.com/whoshuu/cpr/zip/1.3.0
+fn_get_zip https://github.com/whoshuu/cpr/archive/1.3.0.zip
 
-fn_get_zip "$PWD" https://codeload.github.com/rbock/sqlpp11/zip/0.57
-fn_get_zip "$PWD" https://codeload.github.com/HowardHinnant/date/zip/v2.4.1
-fn_get_zip "$PWD" https://codeload.github.com/matthijs/sqlpp11-connector-postgresql/zip/5e834b311484a2f59a94e555723fb579fd0d95d9
+fn_get_zip https://github.com/rbock/sqlpp11/archive/0.57.zip
+fn_get_zip https://github.com/HowardHinnant/date/archive/v2.4.1.zip
+fn_get_zip https://codeload.github.com/matthijs/sqlpp11-connector-postgresql/zip/5e834b311484a2f59a94e555723fb579fd0d95d9
 mv src/sqlpp11-connector-postgresql-5e834b311484a2f59a94e555723fb579fd0d95d9 src/sqlpp11-connector-postgresql_5e834b3
 
-echo -e " * \\033[1;33m"'https://raw.githubusercontent.com/nlohmann/json/v3.5.0/single_include/nlohmann/json.hpp'"\\033[0;39m"
+fn_get json.hpp https://github.com/nlohmann/json/releases/download/v3.5.0/json.hpp
 mkdir json
-curl -s "https://raw.githubusercontent.com/nlohmann/json/v3.5.0/single_include/nlohmann/json.hpp" --output json/json.hpp
+cp src/json.hpp json
